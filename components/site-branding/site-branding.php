f1188a605dafdac54d1641f91137b921328f63bc
Pea Lutz <pea@glocal.coop>
2016-06-21 02:28:58 -0400

    Customized site-branding component to remove top social nav.

---
+++ components/site-branding/site-branding.php
@@ -0,0 +1,11 @@
+<div class="site-branding">
+    <div class="site-branding-container">
+        
+        <?php get_template_part( 'components/site-logo/site-logo' ); ?>
+
+        <?php do_action ( 'anp_site_description' );?>
+
+        <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+        
+    </div>
+</div><!-- .site-branding -->
